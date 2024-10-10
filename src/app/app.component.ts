5b8b129d3a216dda9e079d487437914f539395e3
marcelnoehre <marcelnoehre@web.de>
2024-10-10 23:52:04 +0200

    integrate mat-sidenav + mat-toolbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,37 @@
-import { AfterViewInit, Component } from '@angular/core';
+import {MediaMatcher} from '@angular/cdk/layout';
+import {ChangeDetectorRef, Component, OnDestroy, inject} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ScreenDirective } from './directives/screen.directive';
+import {MatListModule} from '@angular/material/list';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ScreenDirective],
-  providers: [ScreenDirective],
+  imports: [RouterOutlet, MatToolbarModule, MatButtonModule, MatIconModule, MatSidenavModule, MatListModule],
+  providers: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements AfterViewInit {
+export class AppComponent implements OnDestroy {
   title = 'documentation';
-  public screenWidth: number = 0;
-  public screenHeight: number = 0;
+  public mobileQuery!: MediaQueryList;
+  public fillerNav = Array.from({length: 50}, (_, i) => `Nav Item ${i + 1}`);
 
-  ngAfterViewInit(): void {
-    try {
-      this.screenWidth = window.innerWidth;
-      this.screenHeight = window.innerHeight;
-    } catch (err) { }
+  private _mobileQueryListener: () => void;
+
+  constructor() {
+    const changeDetectorRef = inject(ChangeDetectorRef);
+    const media = inject(MediaMatcher);
+
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
   }
-  
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
 }
