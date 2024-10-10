f013984147b43800adda41aa01fbe82b08ca4937
marcelnoehre <marcelnoehre@web.de>
2024-10-10 22:32:18 +0200

    coming-soon screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ScreenDirective } from './directives/screen.directive';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ScreenDirective],
+  providers: [ScreenDirective],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'documentation';
+export class AppComponent implements AfterViewInit {
+  title = 'playground';
+  public screenWidth: number = 0;
+  public screenHeight: number = 0;
+
+  ngAfterViewInit(): void {
+    try {
+      this.screenWidth = window.innerWidth;
+      this.screenHeight = window.innerHeight;
+    } catch (err) { }
+  }
+  
 }
