7a73b9ea8e459639e85818eeda765d57c967013a
marcelnoehre <marcelnoehre@web.de>
2024-10-11 15:15:57 +0200

    store content structure

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Language } from '../interfaces/language.interface';
+import { Content } from '../interfaces/content.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -20,9 +21,61 @@ export class DataService {
     name: 'Deutsch'
   }];
 
-  constructor() { }
+  private content: Content[] = [{
+    title: 'GETTING_STARTED',
+    path: 'getting-started',
+    content: [{
+      title: 'INSTALLATION',
+      path: 'installation',
+    }, {
+      title: 'USAGE',
+      path: 'usage'
+    }, {
+      title: 'FIRST_PROGRAM',
+      path: 'first-program'
+    }]
+  }, {
+    title: 'PROGRAMMING',
+    path: 'programming',
+    content: [{
+      title: 'VARIABLES',
+      path: 'variables'
+    }, {
+      title: 'OPERATORS',
+      path: 'operators'
+    }, {
+      title: 'CONTROL_STRUCTURES',
+      path: 'control-structures'
+    }, {
+      title: 'FUNCTIONS',
+      path: 'functions'
+    }, {
+      title: 'LISTS',
+      path: 'lists'
+    }, {
+      title: 'OBJECTS',
+      path: 'objects'
+    }]
+  }, {
+    title: 'COMMON_ERRORS',
+    path: 'common-errors'
+  }];
+
+  constructor() {}
 
   getLanguages(): Language[] {
     return this.languages;
+  }
+
+  buildPath(contentItem: Content, parentPath: string = ''): string {
+    const fullPath = `${parentPath}/${contentItem.path}`;
+    
+    if (contentItem.content) {
+      contentItem.content.forEach(subContent => {
+        this.buildPath(subContent, fullPath);
+      });
+    }
+    
+    return fullPath;
   }
 }
