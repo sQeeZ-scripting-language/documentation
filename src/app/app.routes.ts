109fb5b420fa8eb36f915067d17be1bb477b110a
marcelnoehre <marcelnoehre@web.de>
2024-10-11 12:05:10 +0200

    setup routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { MainComponent } from './components/main/main.component';
 
-export const routes: Routes = [{ path: '**', redirectTo: '' }];
+export const routes: Routes = [
+    { path: '', component: MainComponent },
+    { path: '**', redirectTo: '' }
+];
