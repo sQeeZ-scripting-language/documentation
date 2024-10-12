2612d0aecbf01fbc32a7ef3615b0273bdbcf0ba6
marcelnoehre <marcelnoehre@web.de>
2024-10-12 10:05:48 +0200

    added key components to main page

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { MatDividerModule } from '@angular/material/divider';
 import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [TranslateModule],
+  imports: [TranslateModule, MatDividerModule],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
